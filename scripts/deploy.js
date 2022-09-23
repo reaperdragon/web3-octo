fe20e456446160a6d535466c58ee45d1c37dba0a
aakrut <aakrutdabhi@gmail.com>
2022-09-23 15:41:09 +0530

    added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
+const { ethers } = require("hardhat");
 
+async function main() {
+  const contractFactory = await ethers.getContractFactory("BlogApp");
+  const contractDeploy = await contractFactory.deploy("Mirror Blog App");
+  await contractDeploy.deployed();
+
+  console.log(`Contract Deployed at: ${contractDeploy.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
