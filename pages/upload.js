c3b2ccbf336063d6bac4ea085dcbbb2b5552832d
aakrut <aakrutdabhi@gmail.com>
2022-09-24 18:57:23 +0530

    added cover image logic

--- pages/upload.js
+++ pages/upload.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import "easymde/dist/easymde.min.css";
+
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
@@ -14,13 +15,47 @@ const Upload = () => {
     cover: "",
   });
 
+  const imageCoverRef = useRef();
+
+  const triggerOnChangeCover = () => {
+    imageCoverRef.current.click();
+  };
+
+
+  const handleFileChange = (e) => {
+    const uploadFile = e.target.files[0];
+    if (!uploadFile) return;
+    setBlog({ ...blog, cover: uploadFile });
+  };
+
+  console.log(blog.cover)
+
   return (
     <div className="">
+      <input
+        id="selectImage"
+        style={{ display: "none" }}
+        type="file"
+        onChange={handleFileChange}
+        ref={imageCoverRef}
+      />
+
+      <div className="max-w-[1440px] mt-2 mb-0 mx-auto">
+        {blog.cover && (
+          <img
+            src={window.URL.createObjectURL(blog.cover)}
+            alt="image"
+            ref={blog.cover}
+            className="w-full h-[420px] rounded-md "
+          />
+        )}
+        <button onClick={triggerOnChangeCover}>Select Cover Image</button>
+      </div>
       <div className="flex flex-col max-w-[1440px] my-0 mx-auto">
         <input
           name="title"
           placeholder="Title Goes Here"
-          className=""
+          className="bg-transparent font-body mt-3 p-1"
           value={blog.title}
           onChange={(e) => setBlog({ ...blog, title: e.target.value })}
         />
