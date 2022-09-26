5d2dd5bb6e8e63dd199d6e58f7debab4a0627c03
aakrut <aakrutdabhi@gmail.com>
2022-09-26 17:03:45 +0530

    added apollo graphql

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,30 @@ import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import BundlrContextProvider from "../context/bundlrContext";
+import { ApolloProvider } from "@apollo/client";
+import client from "../client";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <BundlrContextProvider>
-        <Component {...pageProps} />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
-      </BundlrContextProvider>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <BundlrContextProvider>
+          <Component {...pageProps} />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </BundlrContextProvider>
+      </div>
+    </ApolloProvider>
   );
 }
 
