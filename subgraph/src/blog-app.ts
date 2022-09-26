0983ec89c01d015e389e6b9b217837d0026885c9
aakrut <aakrutdabhi@gmail.com>
2022-09-26 16:12:27 +0530

    added subgraph

--- subgraph/src/blog-app.ts
+++ subgraph/src/blog-app.ts
@@ -13,17 +13,20 @@ export function handleBlogCreated(event: BlogCreatedEvent): void {
   blog.date = event.params.date;
   blog.user = event.params.user;
   blog.createdAt = event.block.timestamp;
+  blog.updatedAt = event.block.timestamp;
   blog.save();
 }
 
 export function handleBlogUpdated(event: BlogUpdatedEvent): void {
   let blog = Blog.load(event.params.blogId.toString());
-  blog!.blogcoverhash = event.params.blogcoverhash;
-  blog!.blogtitle = event.params.blogtitle;
-  blog!.blogcontent = event.params.blogcontent;
-  blog!.category = event.params.category;
-  blog!.date = event.params.date;
-  blog!.user = event.params.user;
-  blog!.updatedAt = event.block.timestamp;
-  blog!.save();
+  if (blog) {
+    blog.blogcoverhash = event.params.blogcoverhash;
+    blog.blogtitle = event.params.blogtitle;
+    blog.blogcontent = event.params.blogcontent;
+    blog.category = event.params.category;
+    blog.date = event.params.date;
+    blog.user = event.params.user;
+    blog.updatedAt = event.block.timestamp;
+    blog.save();
+  }
 }
