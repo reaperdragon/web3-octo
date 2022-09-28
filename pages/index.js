2da08b7caec5738174fc8e9e783fc46fdec44af5
aakrut <aakrutdabhi@gmail.com>
2022-09-28 16:35:21 +0530

    style fixed

--- pages/index.js
+++ pages/index.js
@@ -39,8 +39,8 @@ export default function Home() {
       ) : (
         <div className="h-screen flex items-center  font-body justify-center text-center">
           <div>
-            <h3 className="h-[160px] animate-bounce">🐙</h3>
-            <h2 className="text-[80px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-800 my-2 leading-[120px] sm:text-[50px] md:leading-normal">
+            <h3 className="h-[50px] animate-bounce text-3xl">🐙</h3>
+            <h2 className="text-[80px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-800 my-2 leading-[120px] sm:text-[50px] md:leading-normal sm:text-[30px]">
               The Web 3 Publishing Hub{" "}
             </h2>
             <p className="semibold text-gray-500 my-[26px] font-medium text-3xl sm:text-2xl">
